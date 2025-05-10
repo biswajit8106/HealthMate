b7c7ad7684eb00aeafd51f31f6a3ed8ac29fb218
biswajit8106 <dhacker7777@gmail.com>
2025-05-10 15:15:14 +0530

    commit

---
+++ frontend/build/precache-manifest.2c9f464b8ad5676ddcc3e36c21b650b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32333340319a7735c3b82634c854a67f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c44222ac9e5389bbde34",
+    "url": "/static/css/main.021675b3.chunk.css"
+  },
+  {
+    "revision": "095436080d11f315c738",
+    "url": "/static/js/2.33591b90.chunk.js"
+  },
+  {
+    "revision": "c44222ac9e5389bbde34",
+    "url": "/static/js/main.634f9438.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0a74112b7b92bac2c9e8d46b0ee6e2d9",
+    "url": "/static/media/img4.0a74112b.jpg"
+  },
+  {
+    "revision": "2b3c0af6b6421bae2a6bb8783cadd52d",
+    "url": "/static/media/logo.2b3c0af6.png"
+  }
+]);
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -1,34 +1,39 @@
-// Basic service worker for push notifications
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
-self.addEventListener('push', function(event) {
-  console.log('[Service Worker] Push Received.');
-  let data = {};
-  if (event.data) {
-    try {
-      data = event.data.json();
-    } catch (e) {
-      console.warn('Push event data is not JSON, using text instead');
-      data = { title: 'Medication Reminder', body: event.data.text() };
-    }
-    console.log('[Service Worker] Push data:', data);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.2c9f464b8ad5676ddcc3e36c21b650b3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
   }
-  const title = data.title || 'Medication Reminder';
-  const options = {
-    body: data.body || 'You have a medication reminder.',
-    icon: '/logo192.png',
-    badge: '/logo192.png'
-  };
-  event.waitUntil(self.registration.showNotification(title, options));
 });
 
-self.addEventListener('notificationclick', function(event) {
-  console.log('[Service Worker] Notification click Received.');
-  event.notification.close();
-  event.waitUntil(
-    clients.matchAll({ type: 'window' }).then(function(clientList) {
-      if (clients.openWindow) {
-        return clients.openWindow('/');
-      }
-    })
-  );
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
